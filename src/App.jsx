a938a1721f489621570d3b3789ef7202674bc96b
Joshua.kelbie <joshua.kel@chonvc.ac.th>
2024-10-25 15:51:11 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+function App() {
+
+  return (
+    <>
+      <h2>---------------------</h2>
+      <h1>Joshua kelbie</h1>
+      <h2>---------------------</h2>
+
+      <div>
+      <h1>1</h1>
+      <h2>2</h2>
+      <h3>3</h3>
+      <h4>4</h4>
+      <h5>5</h5>
+      </div>
+      <br />
+      <span>
+      <h1>1</h1>
+      <h2>2</h2>
+      <h3>3</h3>
+      <h4>4</h4>
+      <h5>5</h5>
+      </span>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam expedita iste excepturi, reiciendis iusto vel, inventore deleniti et earum eligendi culpa nam aliquid quos! Culpa eius blanditiis quis placeat illo.</p>
+
+      <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto libero ab sapiente velit facere, id atque illo eligendi. Qui sunt excepturi rerum tempora suscipit magnam dolore nostrum veritatis et voluptas.</span>
+
+
+    </>
+  )
+}
+
+export default App
